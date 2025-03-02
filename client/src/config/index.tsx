eeb4ad16bea43a54abbb5b2c3804f0cf8ea24804
unknown <gitit1@msn.com>
2025-03-02 03:35:46 +0200

    updated local server to dev

--- client/src/config/index.tsx
+++ client/src/config/index.tsx
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: process.env.BASE_URL || 'http://localhost:3031'
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3031'
 };
 
 export default config;
